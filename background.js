e22209f53191af01aab2915af4dc93a1ff6fc10d
Danny Summerlin <danny.summerlin@jstart.org>
2019-03-10 23:27:31 -0400

    not working yet, promises not resolving right to get session info

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
 const debug = true
-var log = (msg)=> {if(debug) console.log(msg)}
+var log = msg=>{if(debug) console.log(msg)}
 var tasks = {},
-	orgId, // retrieve or allow multiple?
+	orgId = "00D540000000kjy", // retrieve or allow multiple?
 	sessionId,
 	apiUrl,
 	userId,
-	lastUpdateTimestamp
+	lastUpdateTimestamp,
+	tryCount = 0
 const regMatchOrgId = /sid=([\w\d]+)/
 const regMatchSid = /sid=([a-zA-Z0-9\.\!]+)/
 const SFAPI_VERSION = 'v40.0'
 
-let promiseHttp = (args)=>{
+var promiseHttp = (args)=>{
 	args = Object.assign({ targetUrl: "", type: "json", headers: {}, data: {}, method: "GET"}, args)
 	let request = { method: args.method, headers: args.headers }
 	if(args.targetUrl.substring(0,5) != "https")
@@ -30,24 +31,25 @@ let promiseHttp = (args)=>{
 			return data
 	})
 }
-let getSessionInfo = (resolve)=>{
-	chrome.cookies.getAll({}, (all)=>{
-		all.forEach((c)=>{
-			if(c.domain.includes("force.com") && (c.value.includes(orgId) || c.name == "disco")) {
-				if(c.name == 'sid') {
-					sessionId = c.value
-					apiUrl = c.domain
+var getSessionInfo = ()=>{
+log("get session info")
+	return new Promise(resolve => {
+		resolve(chrome.cookies.getAll({}, (all)=>{
+			all.forEach((c)=>{
+				if(c.domain.includes("force.com") && (c.value.includes(orgId) || c.name == "disco")) {
+					if(c.name == 'sid') {
+						sessionId = c.value
+						apiUrl = c.domain
+					}
+					else if(c.name == 'disco') userId = c.value.match(/005[\w\d]+/)[0]
 				}
-				else if(c.name == 'disco') userId = c.value.match(/005[\w\d]+/)[0]
-			}
+			})
 		})
-	})
-	if(resolve)
-		resolve()
+	)})
 }
-let getUserId = ()=>userId
-let completeTask = taskId=>{}
-let createTask = subject=>{
+var getUserId = ()=>userId
+var completeTask = taskId=>{}
+var createTask = subject=>{
 	if(subject != "" && getUserId()) {
 		promiseHttp({
 			method: "POST",
@@ -63,36 +65,34 @@ let createTask = subject=>{
 		})
 	}
 }
-let fetchTasks = ()=>{
-	promiseHttp({
-		url: "https://" + apiUrl + "/services/data/" + SFAPI_VERSION + "/tooling/query/?q=SELECT+Id,+Subject+FROM+Task+WHERE+OwnerId='" + getUserId() + "'+and+isclosed=false",
-		headers: {"Authorization": "Bearer " + sessionId, "Content-Type": "application/json" })
-	})
-	.then(function(success) {
+var fetchTasks = ()=>{
+log("fetch tasks")
+	return new Promise(resolve => {
+		let args = {
+			url: "https://" + apiUrl + "/services/data/" + SFAPI_VERSION + "/tooling/query/?q=SELECT+Id,+Subject+FROM+Task+WHERE+OwnerId='" + getUserId() + "'+and+isclosed=false",
+			headers: {"Authorization": "Bearer " + sessionId, "Content-Type": "application/json" }
+		}
+		resolve(promiseHttp(args))
+	}).then(function(success) {
+log(success)
 		let numberOfUserRecords = success.records.length
 		for (var i = 0; i < success.records.length; i++) {
 			success.records[i].Subject
 		}
 		lastUpdateTimestamp = new Date
+		tryCount = 0
 	}).catch(function(error) {
-		Promise(resolve=>getSessionInfo(resolve)).then(fetchTasks)
+		if(tryCount < 3) {
+log(error)
+			tryCount++
+			return new Promise(resolve=>resolve(getSessionInfo)).then(fetchTasks)
+		}
 	})
 }
-let refreshTaskList = tab=>
+var refreshTaskList = tab=>
 	chrome.tabs.sendMessage(tab.id, {action: "refreshTaskList", tasks: tasks}, response => console.log(response))
 
-let init = tab=>{
-	return new Promise(resolve=>resolve(getSessionInfo())).then(fetchTasks).then(refreshTaskList.bind(null, tab))
+var init = tab=>{
+	getSessionInfo().then(fetchTasks).then(refreshTaskList.bind(null, tab))
 }
-chrome.tabs.onCreated.addListener(tab=>{
-	init(tab)
-})
-// chrome.commands.onCommand.addListener((command)=>{
-// 	switch(command) {
-// 	}
-// })
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
-// 	switch(request.action) {
-// 	}
-// 	return true
-// })
+chrome.tabs.onCreated.addListener(tab=>init(tab))
