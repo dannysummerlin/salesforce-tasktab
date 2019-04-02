b7784032c5d670e4911e37791ab091eabd902126
Danny Summerlin <danny.summerlin@jstart.org>
2019-04-01 23:47:28 -0400

    fixing bad merge
    need to rebase the rest

--- shared.js
+++ shared.js
@@ -1,1 +1,36 @@
 let taskLimit = 50
+
+let themes = {
+	base: {
+		name: "theme-base",
+		primary: "rgba(176,196,223,1)",
+		primaryTransparent: "rgba(176,196,223,0.5)",
+		secondary: "rgba(27,95,158,1)",
+		secondaryTransparent: "rgba(27,95,158,0.05)",
+		backgroundImage: "linear-gradient(0deg, rgba(176,196,223,1) 70%, rgba(27,95,158,1) 100%)"
+	},
+	emoji: {
+		name: "theme-emoji",
+		primary: "rgba(223,176,196,1)",
+		primaryTransparent: "rgba(223,176,196,0.5)",
+		secondary: "rgba(158,27,95,1)",
+		secondaryTransparent: "rgba(158,27,95,0.05)",
+		backgroundImage: "linear-gradient(0deg, rgba(223,176,196,1) 70%, rgba(158,27,95,1) 100%)"
+	},
+	terminal: {
+		name: "theme-terminal",
+		primary: "rgba(0,160,0,1)",
+		primaryTransparent: "rgba(0,160,0,0.5)",
+		secondary: "rgba(32, 204, 32, 1)",
+		secondaryTransparent: "rgba( 32, 204, 32,0.05)",
+		backgroundImage: "radial-gradient(circle, rgba(50,50,50,0.92) 70%, rgba(0,0,0,1) 100%)"
+	},
+	simple: {
+		name: "theme-simple",
+		primary: "rgba(200,200,200,1)",
+		primaryTransparent: "rgba(200,200,200,0.3)",
+		secondary: "rgba(170,170,170, 1)",
+		secondaryTransparent: "rgba(170,170,170,0.05)",
+		backgroundImage: "linear-gradient(0deg, rgba(0,0,0,0.8) 40%, rgba(0,0,0,1) 100%)"
+	}
+}
